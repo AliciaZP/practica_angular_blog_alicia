e6d6f6c031d4542c8d46fb1c8c4895f63be23002
Alicia Zamorano <azpans@hotmail.com>
2023-11-12 20:37:59 +0100

    fix: fix images from db

--- src/app/db/posts.db.ts
+++ src/app/db/posts.db.ts
@@ -4,80 +4,80 @@ export const POSTS: Post[] = [
     {
         titulo: "Teoría del Rojo",
         texto: "El rojo es un color cálido y apasionado que evoca emociones fuertes y enérgicas. Este tono vibrante ha sido significativo a lo largo de la historia, simbolizando una amplia gama de conceptos y sentimientos. En la naturaleza, el rojo se asocia con elementos poderosos como el fuego y la sangre, transmitiendo un sentido de vitalidad y peligro.Culturalmente, el rojo ha sido utilizado en ceremonias y celebraciones para representar la buena suerte y la prosperidad.\nEn el mundo del arte, numerosos artistas han empleado el rojo para destacar y expresar emociones intensas.En términos de diseño, el rojo es a menudo utilizado para captar la atención del espectador y transmitir una sensación de urgencia.Desde el punto de vista psicológico, el rojo se ha asociado con la estimulación del apetito y la activación del sistema nervioso.En general, la teoría del color atribuye al rojo cualidades emocionales y físicas que lo convierten en un elemento fascinante y versátil.",
-        autor: "Experto en colores",
-        imagen: "https://i.pinimg.com/originals/2b/81/0a/2b810a99ff502c1719ee900564b16878.jpg",
+        autor: "Alicia",
+        imagen: "https://i.postimg.cc/SQPxnRPj/post-rojo-min.png",
         fecha: new Date(2023, 1, 30),
         categoria: "Teoría del Color"
     },
     {
         titulo: "El Significado del Azul",
         texto: "El azul, un color refrescante y sereno, ha cautivado a la humanidad a lo largo de la historia.Presente en el cielo y el océano, el azul evoca una sensación de amplitud y calma.Psicológicamente, se le atribuyen propiedades relajantes y pacificadoras, siendo un color comúnmente asociado con la estabilidad emocional.En el ámbito del diseño, el azul se utiliza para transmitir confianza y profesionalismo.Empresas y marcas a menudo eligen este tono para construir una imagen de fiabilidad y seguridad.Además, el azul tiene connotaciones simbólicas, representando la lealtad y la verdad en diversas culturas.A través de estas distintas dimensiones, el azul se revela como un color polifacético que influye tanto en nuestras percepciones emocionales como en nuestras decisiones prácticas.",
-        autor: "Especialista en psicología del color",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Alicia",
+        imagen: "https://i.postimg.cc/tTC4xxyn/post-azul-min.png",
         fecha: new Date(2022, 1, 30),
         categoria: "Teoría del Color"
     },
     {
         titulo: "Verde y Naturaleza",
         texto: "El verde, con su conexión innata con la naturaleza, evoca una sensación de frescura y renovación.Este color, abundante en bosques y paisajes, ha sido históricamente asociado con la vida y la vitalidad.Desde el punto de vista psicológico, el verde se percibe como calmante y equilibrado, siendo capaz de aliviar el estrés y promover la armonía.En el diseño, el verde se utiliza a menudo para transmitir mensajes relacionados con la sostenibilidad y la ecología.Empresas comprometidas con la responsabilidad ambiental incorporan el verde en sus identidades visuales para comunicar su compromiso con la naturaleza.Culturalmente, el verde también ha sido un símbolo de buena suerte en muchas sociedades, asociado con la fertilidad y la prosperidad.En resumen, el verde es un color versátil que va más allá de su conexión visual con la naturaleza, influyendo en nuestras emociones y decisiones.",
-        autor: "Ecologista de colores",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Paquito",
+        imagen: "https://i.postimg.cc/65G5nwsc/post-verde-min.png",
         fecha: new Date(2021, 1, 30),
         categoria: "Naturaleza"
     },
     {
         titulo: "Amarillo: La Luz del Sol",
         texto: "El amarillo, con su luminosidad y calidez, representa la luz del sol y la alegría que trae consigo.Este color vibrante tiene la capacidad de atraer la atención y transmitir energía positiva.Psicológicamente, se asocia con la felicidad y la creatividad, estimulando la mente y promoviendo el optimismo.En diseño, el amarillo se utiliza estratégicamente para resaltar elementos importantes y captar la mirada del observador.Culturalmente, el amarillo ha tenido diversas connotaciones, desde la representación del lujo hasta la simbolización de la renovación.Este color también ha sido utilizado en la señalización y advertencias debido a su alta visibilidad.En resumen, el amarillo es un color que ilumina nuestro entorno, aportando una dosis de vitalidad y positividad a nuestras experiencias diarias.",
-        autor: "Especialista en luminosidad",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Paquito",
+        imagen: "https://i.postimg.cc/ZWY4pGkz/post-amarillo-min.png",
         fecha: new Date(2020, 1, 30),
         categoria: "Naturaleza"
     },
     {
         titulo: "Naranja y Creatividad",
         texto: "El naranja, una mezcla de la energía del rojo y la felicidad del amarillo, es un color cálido y enérgico que evoca creatividad y diversión. Psicológicamente, se asocia con la estimulación y el entusiasmo, inspirando ideas frescas y nuevas perspectivas. En diseño, el naranja se utiliza para resaltar elementos y agregar un toque juguetón a la composición visual. Culturalmente, este color ha sido asociado con la renovación y la positividad, utilizado en festivales y celebraciones.En la naturaleza, el naranja se presenta en tonos de puestas de sol y frutas maduras, transmitiendo una sensación de calidez y vitalidad.  En resumen, el naranja es un color vibrante que despierta la imaginación y añade un toque de energía positiva a nuestras vidas cotidianas. ",
-        autor: "Creativo en colores",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Paquito",
+        imagen: "https://i.postimg.cc/fy1zpqdY/post-naranja-min.png",
         fecha: new Date(2019, 1, 30),
         categoria: "Teoría del Color"
     },
     {
         titulo: "Violeta y Misterio",
         texto: "El violeta, con su misterioso encanto, es un color asociado con la creatividad y la espiritualidad. Psicológicamente, se cree que tiene propiedades que estimulan la imaginación y la intuición.En diseño, el violeta se utiliza para agregar un toque de elegancia y sofisticación, a menudo asociado con productos de alta calidad.Culturalmente, el violeta ha sido vinculado a la realeza y la nobleza, siendo utilizado en la vestimenta y símbolos de estatus.En la naturaleza, se presenta en flores exquisitas, transmitiendo una sensación de belleza y encanto.En resumen, el violeta es un color que despierta la curiosidad y añade un toque de misterio y magia a nuestras experiencias visuales.",
-        autor: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
-        imagen: "url_de_la_imagen_violeta.jpg",
+        autor: "Alicia",
+        imagen: "https://i.postimg.cc/vmGQJV42/post-violeta-min.png",
         fecha: new Date(2018, 1, 30),
         categoria: "Teoría del Color"
     },
     {
         titulo: "Rosado y Dulzura",
         texto: "El rosado, con su suavidad y feminidad, evoca una sensación de dulzura y ternura.Psicológicamente, se asocia con la calma y la relajación, transmitiendo una sensación de cuidado y afecto.En diseño, el rosado se utiliza para crear ambientes delicados y agradables, siendo popular en productos dirigidos a un público femenino.Culturalmente, este color ha sido vinculado a la inocencia y la juventud, utilizado en juguetes y artículos infantiles.En la naturaleza, el rosado se presenta en puestas de sol y flores encantadoras, añadiendo un toque de belleza sutil.En resumen, el rosado es un color que suaviza nuestras experiencias visuales y evoca sentimientos de cariño y tranquilidad.",
-        autor: "Especialista en colores dulces",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Anónimo",
+        imagen: "https://i.postimg.cc/zGZDkM8r/post-rosa-min.png",
         fecha: new Date(2017, 1, 30),
         categoria: "Sentimientos"
     },
     {
         titulo: "Marrón y Naturalidad",
         texto: "El marrón, un color terroso que evoca la naturaleza y la estabilidad, ha sido valorado a lo largo de la historia.Culturalmente, se asocia con la tierra y la conexión con la naturaleza, transmitiendo una sensación de calidez y seguridad.Psicológicamente, el marrón se percibe como sólido y confiable, siendo utilizado en diseño para agregar un toque de autenticidad.En la moda, el marrón se asocia con la elegancia clásica y la simplicidad atemporal.En la naturaleza, este color se presenta en tierras fértiles y árboles robustos, añadiendo una sensación de arraigo y solidez.En resumen, el marrón es un color que nos conecta con la tierra y evoca una sensación de naturalidad y estabilidad. ",
-        autor: "Amante de la tierra y los colores",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Paquito",
+        imagen: "https://i.postimg.cc/XJH7cY7H/post-marron-min.png",
         fecha: new Date(2016, 1, 30),
         categoria: "Teoría del Color"
     },
     {
         titulo: "Gris y Sofisticación",
         texto: "El gris, un color neutro y versátil, representa la neutralidad y la sofisticación.En el diseño, el gris se utiliza para transmitir profesionalismo y elegancia, siendo común en contextos empresariales y minimalistas.Psicológicamente, se asocia con la estabilidad y la madurez, añadiendo un toque de seriedad a la paleta de colores.En la moda, el gris es un color atemporal que puede adaptarse a diversas ocasiones y estilos.Este tono sutil también se utiliza para resaltar otros colores en composiciones visuales.En resumen, el gris es un color que equilibra la expresión visual, añadiendo un toque de refinamiento y sofisticación a nuestras percepciones.",
-        autor: "Experto en colores neutros",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Anónimo",
+        imagen: "https://i.postimg.cc/C5RMvpRF/post-gris-min.png",
         fecha: new Date(2015, 1, 30),
         categoria: "Sentimientos"
     },
     {
         titulo: "Negro Elegante",
         texto: "El negro, un color elegante y clásico, ha sido un elemento fundamental en el mundo del diseño y la moda.Culturalmente, se asocia con la formalidad y el misterio, transmitiendo una sensación de autoridad y poder.En la moda, el negro es un color versátil que se utiliza para crear looks sofisticados y atemporales.En diseño gráfico, el negro se utiliza para resaltar elementos y añadir un toque de elegancia a las composiciones visuales.Psicológicamente, se percibe como fuerte y protector, siendo un color que aporta una sensación de seguridad.En resumen, el negro es un color que eleva la estética y agrega un toque de elegancia y misterio a nuestras experiencias visuales.",
-        autor: "Estilista del negro",
-        imagen: "https://www.researchgate.net/publication/281089718/figure/fig5/AS:284478913171468@1444836466666/The-Colour-Wheel-Image-with-permission-from-wwwartyfactorycom.png",
+        autor: "Alicia",
+        imagen: "https://i.postimg.cc/JhQh0qwW/post-negro-min.png",
         fecha: new Date(2014, 1, 30),
         categoria: "Sentimientos"
     },
