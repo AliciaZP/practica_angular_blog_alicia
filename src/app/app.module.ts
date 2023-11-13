2c7aa1d8537c30f6395cf036bfc2947f21346f94
Alicia Zamorano <azpans@hotmail.com>
2023-11-13 21:12:08 +0100

    feat: add quill texto enriquecido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DetallePostComponent } from './pages/detalle-post/detalle-post.componen
 import { HomeComponent } from './pages/home/home.component';
 import { ErrorComponent } from './pages/error/error.component';
 
+import { EditorModule } from 'primeng/editor';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ErrorComponent } from './pages/error/error.component';
   imports: [
     ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
